0d157dbd7038b92e381ff5cb3340cca267c7eabc
youfeng <youfeng1024@gmail.com>
2024-12-11 19:11:02 +0000

    2024年12月12日 03:11

--- util/DropExtremeLabel.py
+++ util/DropExtremeLabel.py
@@ -11,96 +11,97 @@ from qlib.utils.serial import Serializable
 from qlib.utils.paral import datetime_groupby_apply
 from qlib.data.inst_processor import InstProcessor
 from qlib.data import D
+from qlib.data.dataset.processor import Processor, get_group_columns
 import logging
 
 
-def get_group_columns(df: pd.DataFrame, group: Union[Text, None]):
-    """
-    get a group of columns from multi-index columns DataFrame
-    从多重索引列DataFrame中获取一组列
+# def get_group_columns(df: pd.DataFrame, group: Union[Text, None]):
+#     """
+#     get a group of columns from multi-index columns DataFrame
+#     从多重索引列DataFrame中获取一组列
 
-    Parameters
-    ----------
-    df : pd.DataFrame
-        with multi of columns.
-        具有多重列的DataFrame
-    group : str
-        the name of the feature group, i.e. the first level value of the group index.
-        特征组的名称，即组索引的第一级值
-    """
-    if group is None:
-        return df.columns
-    else:
-        return df.columns[df.columns.get_loc(group)]
+#     Parameters
+#     ----------
+#     df : pd.DataFrame
+#         with multi of columns.
+#         具有多重列的DataFrame
+#     group : str
+#         the name of the feature group, i.e. the first level value of the group index.
+#         特征组的名称，即组索引的第一级值
+#     """
+#     if group is None:
+#         return df.columns
+#     else:
+#         return df.columns[df.columns.get_loc(group)]
 
 
-class Processor(Serializable):
-    def fit(self, df: pd.DataFrame = None):
-        """
-        learn data processing parameters
-        学习数据处理参数
+# class Processor(Serializable):
+#     def fit(self, df: pd.DataFrame = None):
+#         """
+#         learn data processing parameters
+#         学习数据处理参数
 
-        Parameters
-        ----------
-        df : pd.DataFrame
-            When we fit and process data with processor one by one. The fit function reiles on the output of previous
-            processor, i.e. `df`.
-            当我们逐个使用处理器拟合和处理数据时，fit函数依赖于前一个处理器的输出，即`df`
-        """
+#         Parameters
+#         ----------
+#         df : pd.DataFrame
+#             When we fit and process data with processor one by one. The fit function reiles on the output of previous
+#             processor, i.e. `df`.
+#             当我们逐个使用处理器拟合和处理数据时，fit函数依赖于前一个处理器的输出，即`df`
+#         """
 
-    @abc.abstractmethod
-    def __call__(self, df: pd.DataFrame):
-        """
-        process the data
-        处理数据
+#     @abc.abstractmethod
+#     def __call__(self, df: pd.DataFrame):
+#         """
+#         process the data
+#         处理数据
 
-        NOTE: **The processor could change the content of `df` inplace !!!!! **
-        注意：**处理器可能会就地更改`df`的内容！！！！**
-        User should keep a copy of data outside
-        用户应该在外部保留数据副本
+#         NOTE: **The processor could change the content of `df` inplace !!!!! **
+#         注意：**处理器可能会就地更改`df`的内容！！！！**
+#         User should keep a copy of data outside
+#         用户应该在外部保留数据副本
 
-        Parameters
-        ----------
-        df : pd.DataFrame
-            The raw_df of handler or result from previous processor.
-            处理器的原始df或来自前一个处理器的结果
-        """
+#         Parameters
+#         ----------
+#         df : pd.DataFrame
+#             The raw_df of handler or result from previous processor.
+#             处理器的原始df或来自前一个处理器的结果
+#         """
 
-    def is_for_infer(self) -> bool:
-        """
-        Is this processor usable for inference
-        此处理器是否可用于推理
-        Some processors are not usable for inference.
-        某些处理器不可用于推理
+#     def is_for_infer(self) -> bool:
+#         """
+#         Is this processor usable for inference
+#         此处理器是否可用于推理
+#         Some processors are not usable for inference.
+#         某些处理器不可用于推理
 
-        Returns
-        -------
-        bool:
-            if it is usable for infenrece.
-            如果它可用于推理则返回True
-        """
-        return True
+#         Returns
+#         -------
+#         bool:
+#             if it is usable for infenrece.
+#             如果它可用于推理则返回True
+#         """
+#         return True
 
-    def readonly(self) -> bool:
-        """
-        Does the processor treat the input data readonly (i.e. does not write the input data) when processing
-        处理器在处理时是否将输入数据视为只读（即不写入输入数据）
+#     def readonly(self) -> bool:
+#         """
+#         Does the processor treat the input data readonly (i.e. does not write the input data) when processing
+#         处理器在处理时是否将输入数据视为只读（即不写入输入数据）
 
-        Knowning the readonly information is helpful to the Handler to avoid uncessary copy
-        了解只读信息有助于Handler避免不必要的复制
-        """
-        return False
+#         Knowning the readonly information is helpful to the Handler to avoid uncessary copy
+#         了解只读信息有助于Handler避免不必要的复制
+#         """
+#         return False
 
-    def config(self, **kwargs):
-        attr_list = {"fit_start_time", "fit_end_time"}
-        for k, v in kwargs.items():
-            if k in attr_list and hasattr(self, k):
-                setattr(self, k, v)
+#     def config(self, **kwargs):
+#         attr_list = {"fit_start_time", "fit_end_time"}
+#         for k, v in kwargs.items():
+#             if k in attr_list and hasattr(self, k):
+#                 setattr(self, k, v)
 
-        for attr in attr_list:
-            if attr in kwargs:
-                kwargs.pop(attr)
-        super().config(**kwargs)
+#         for attr in attr_list:
+#             if attr in kwargs:
+#                 kwargs.pop(attr)
+#         super().config(**kwargs)
 
 
 class DropnaProcessor(Processor):
